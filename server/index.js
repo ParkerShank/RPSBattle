a66764a4693d9474a7dcbb6e883b6e98024c170a
Parker Shanklin <pshanklin1655@gmail.com>
2026-05-05 03:03:05 -0500

    index on wrong port

--- server/index.js
+++ server/index.js
@@ -908,5 +908,5 @@ wss.on('connection', (ws) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
