0475a8d9fdd06dc7e83c54aa0e068215a7e4093d
John Thompson <jwthompson121@gmail.com>
2026-05-04 22:49:27 -0500

    changed the client to connect to the servers public port.

--- client/src/hooks/useGameSocket.js
+++ client/src/hooks/useGameSocket.js
@@ -21,7 +21,7 @@ function useGameSocketProviderValue() {
     // useEffect to establish WebSocket connection when component mounts
   useEffect(() => {
     // create WebSocket connection to server
-    const socket = new WebSocket('ws://localhost:3001')
+    const socket = new WebSocket('ws://3.141.165.85:3000')
     // store socket in ref so we can use it in send function
     ws.current = socket
     // set up event handlers for WebSocket
